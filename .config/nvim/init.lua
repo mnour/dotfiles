804f375d4aacc905175b7868bdf2f9f4aefd998e
Mohammad Noureldin <mnour@mac.home>
2025-05-22 16:16:49 +0200

    Update Neovim Config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
-vim.o.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.cmd('set nocompatible')
+vim.opt.syntax = enable
+vim.opt.filetype = on
+vim.opt.path:append { "**" }
+vim.opt.wildmenu = true
 
