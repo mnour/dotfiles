5a685928248f2125cb264b9b55693ca0c697063a
Mohammad Noureldin <153299+mnour@users.noreply.github.com>
2025-06-16 19:33:55 +0200

    Update Neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,11 @@
+require("config.lazy")
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.cmd('set nocompatible')
+vim.cmd.nocompatible = true
 vim.opt.syntax = enable
 vim.opt.filetype = on
 vim.opt.path:append { "**" }
 vim.opt.wildmenu = true
+vim.cmd.colorscheme("tokyonight-night")
 
